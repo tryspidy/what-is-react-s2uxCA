aa7030c185d0a5b8505d40ea024d7adbc2feace9
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-17 05:36:52 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+This is a formatted version of Komadori's answer 
+(https://tinyurl.com/Komadori)
+React is a JavaScript library for building user interfaces. 
+It is maintained by Facebook and a community of 
+individual developers and companies. 
+React can be used as a base in the development of single-page 
+or mobile applications.
